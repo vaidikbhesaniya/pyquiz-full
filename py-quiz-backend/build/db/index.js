921d7faf6de90c62218e930ae72592cc5229586a
Mr. V <vaidikbhesaniya1212@gmail.com>
2024-05-11 03:03:20 +0530

    main

--- py-quiz-backend/build/db/index.js
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-function singletonPrismaClient() {
-    return new PrismaClient();
-}
-const prisma = globalThis.prismaGlobal ?? singletonPrismaClient();
-export default prisma;
-if (process.env.NODE_ENV !== "production")
-    globalThis.prismaGlobal = prisma;
